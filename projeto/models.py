658fdfb3fc5deaf2291380558b70ca9f1b1fb987
titione <titionee@gmail.com>
2022-05-02 20:17:39 -0300

    Adicionado relacionamento perfil/projeto

--- projeto/models.py
+++ projeto/models.py
@@ -158,7 +158,7 @@ class Projeto(TimeStampedModel):
         on_delete=models.DO_NOTHING
         )
 
-    perfil = models.ForeignKey(
+    perfil = models.ManyToOneRel(
         Perfil,
         verbose_name=("PERFIL"),
         on_delete=models.DO_NOTHING
