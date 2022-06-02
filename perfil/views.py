b98f0dbf8b8b63982695963b3872952c9109eb8f
Felipe <arandel25@gmail.com>
2022-06-02 01:35:49 -0300

    Perfilview está salvando e editando

--- perfil/views.py
+++ perfil/views.py
@@ -1,6 +1,9 @@
+from ast import Not
 from django.shortcuts import render
+from perfil.models import Perfil
 from perfil.serializers import PerfilSerializer
 from django.contrib.auth.decorators import login_required
+from django.http.response import HttpResponseRedirect
 
 from perfil.service import PerfilService
 
@@ -8,16 +11,31 @@ _PERFIL_SERVICE = PerfilService()
 
 @login_required
 def perfil(request):
-    perfil = _PERFIL_SERVICE.find_by_user(request.user.id)
-    if perfil is None:
-        return render(request, 'perfil.html', {'section': 'perfil'})
-    else:
-        if request.method == 'POST':
-            serializers = PerfilSerializer(data=request.data)
+    if request.method == 'GET':
+        perfil = _PERFIL_SERVICE.find_by_user(request.user.id)
+        if perfil is None:
+            return render(request, 'perfil.html', {'section': 'perfil'})
+        else:
+            return render(request, 'perfil.html', context={"perfil": perfil, 'section': 'perfil'})
             
-            if serializers.is_valid:
-                serializers.save(usuario_id=request.user.id)
-                render(request, 'projetos.html', {'section': 'projetos'})
-            else:
-                render(request, 'perfil.html', context={"perfil": {request.POST.get("perfil")}})
-    return render(request, 'perfil.html', context={"perfil": perfil, 'section': 'perfil'})
+            
+    if request.method == 'POST':
+        perfil = _PERFIL_SERVICE.find_by_user(request.user.id)
+        if perfil is None:
+            perfil = Perfil()
+        perfil.usuario = request.user
+        perfil.bairro = request.POST.get('bairro')
+        perfil.cidade = request.POST.get('cidade')
+        perfil.dirigente = request.POST.get('nome_dirigente')
+        perfil.email_instituicao = request.POST.get('email_instituicao')
+        perfil.fax = request.POST.get('fax')
+        perfil.logradouro = request.POST.get('logradouro')
+        perfil.nome_instituicao = request.POST.get('nome_instituicao')
+        perfil.numero = request.POST.get('numero')
+        perfil.telefone = request.POST.get('telefone')
+        perfil.UF = request.POST.get('UF')
+        perfil.save()
+        return render(request, 'projetos.html', {'section': 'projetos'})
+        
+        # Exemplo:
+        #     render(request, 'perfil.html', context={"perfil": {request.POST.get("perfil")}})
