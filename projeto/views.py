f381ba454e99fa751559d39fbdb9dc1e0c88a0e3
titione <titionee@gmail.com>
2022-06-02 23:08:06 -0300

    Ajustado tela de projetos, adicionado tabela, edição e exclusão

--- projeto/views.py
+++ projeto/views.py
@@ -18,7 +18,7 @@ def projetos(request):
     projetos = _SERVICE_PROJETO.find_by_perfil(user)
     if projetos is None:
         return render(request, 'projetos.html', {'section': 'projetos'})
-    return render(request, 'projetos.html', context={"projetos": projetos})
+    return render(request, 'projetos.html', context={"projetos": projetos, 'section': 'projetos'})
     
 def projeto(request):
     pass
